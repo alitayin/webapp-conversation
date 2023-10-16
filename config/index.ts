d9c4a8d3f348459d29d3fa91b58218c28c96de33
alitayin <105124378+alitayin@users.noreply.github.com>
2023-10-16 17:22:55 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
+  title: 'Avalanche Guide',
+  description: 'This is a bot created by Alita to assist in answering how to set up nodes and staking. It can solve some of your problems. For more professional questions, you can join https://t.me/eCashDevelopment to consult.',
+  copyright: 'alitayin',
   privacy_policy: '',
-  default_language: 'zh-Hans',
+  default_language: 'en',
 }
 
 export const isShowPrompt = false
